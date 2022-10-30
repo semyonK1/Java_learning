597538f46dc00ec92a6ab919258019afe572e1d9
Semyon <k7@semyonp.ru>
2022-10-30 17:54:52 +0300

    single and double nums

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 В программе должен вестись подсчет количества попыток угадывания
 */
 public class Main {
-    public static void main(String[] args) {
 
+    public static void solutionSingleNum() {
         int userNum;
         int count = 0;
 
@@ -33,5 +33,64 @@ public class Main {
         } while(userNum != num);
 
         System.out.println("Число затраченных попыток " + count);
+    }
+
+    public static void solutionDoubleNums() {
+
+        int userNum1 = -1;
+        int userNum2 = -1;
+        int count = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("ЗАДАЧА: Угадать два числа (от 1 до 15 и от" +
+                " 0 до 5");
+
+        int num1 = (int)Math.floor(Math.random() * 15 + 1);
+        int num2 = (int)Math.floor(Math.random() * 6);
+
+        do {
+            count++;
+            if (userNum1 != num1) {
+                System.out.println("Введите ваше первое число: ");
+                userNum1 = input.nextInt();
+                if (userNum1 > num1) {
+                    System.out.println("Число 1 меньше ");
+                } else if (userNum1 < num1) {
+                    System.out.println("Число 1 больше ");
+                } else {
+                    System.out.println("Первое число компьютера" +
+                            " действительно равно: " + userNum1);
+                }
+            }
+            if (userNum2 != num2) {
+                System.out.println("Введите ваше второе число: ");
+                userNum2 = input.nextInt();
+
+                if (userNum2 > num2) {
+                    System.out.println("Число 2 меньше ");
+                } else if (userNum2 < num2) {
+                    System.out.println("Число 2 больше ");
+                } else {
+                    System.out.println("Второе число компьютера" +
+                            " действительно равно: " + userNum2);
+                }
+            }
+        } while (userNum2 != num2 || userNum1 != num1);
+        System.out.println("Число затраченных попыток " + count);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Выберите вариант решения \n" +
+                " ('1' - угадывание одного числа) \n" +
+                " ('2' - угадывание двух чисел)");
+        Scanner input = new Scanner(System.in);
+        int solN = input.nextInt();
+        if (solN == 1) {
+            solutionSingleNum(); // ф-ция с угадыванием одного
+        } else if (solN == 2) {
+            solutionDoubleNums(); // ф-ция с угадыванием двух
+        } else {
+            System.out.println("Такого я не знаю!");
+        }
     }
 }
