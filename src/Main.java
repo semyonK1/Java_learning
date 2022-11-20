786d7f904392c7d34053b4aa45fbf87333f7d98a
Semyon <k7@semyonp.ru>
2022-11-20 17:11:28 +0300

    math

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
+    private String name;
+    static private double y;
+    static private double S = 0;
+    static private double step;
+    static private double a;
+    static private double b;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        a = 0;
+        b = 2;
+        step = 0.0001;
+        for (double x = a; x <= b; x = x + step) {
+            y = f(x);
+            S = S + step * y;
+        }
+        System.out.println("Площадь под графиком: " + S);
+    }
+
+    private static double f(double x) {
+        return x;
     }
 }
